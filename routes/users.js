f6ff20f5b33cd557b0069bd426403e321e688c7a
Richieid23 <fitrarizki23@gmail.com>
2022-09-24 15:13:49 +0700

    add user get list API

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ router.post('/register', usersHandler.register);
 router.post("/login", usersHandler.login);
 router.put("/:id", usersHandler.update);
 router.get("/:id", usersHandler.getUser);
+router.get("/", usersHandler.getUsers);
 
 
 module.exports = router;
